c30406ed5f5fd7d4943a993d316e4e5196f7cbcf
FlutterDeveloper1021 <omkargharge@nimapinfotech.com>
2023-06-22 14:10:34 +0530

    Second day Commit

--- Tutorial1.dart
+++ Tutorial1.dart
@@ -1,22 +1,50 @@
-// void main() {
-//   // print("Hello World!");
-//   // int myInteger = -1;
-//   num myInteger = -1;
-//   double myDouble = 1.2;
-//   String myString = "Omkar Gharge";
-//   bool myBool = true;
+//Data Types
+void main() {
+  // int myInteger = -1;
+  //Integer
+  num myInteger = -1;
+  // Double
+  double myDouble = 1.2;
+  // String
+  String myString = "Omkar Gharge";
 
-//   // if variable hold num as well as double value then use num
-//   myInteger = 1;
-//   print(myInteger);
+  // Boolean
+  bool myBool = true;
 
-//   // here ~ this can remove the decimals and only print the integer
-//   num temp = ((4 + 3) ~/ 4);
-//   print(temp);
-//   print(myDouble);
-//   print(myString);
-//   print(myBool);
-// }
+  // if variable hold num as well as double value then use num
+  myInteger = 1;
+  print(myInteger);
+
+  // here ~ this can remove the decimals and only print the integer
+  // num temp = ((4 + 3) ~/ 4);
+  // print(temp);
+  // print(myDouble);
+  // print(myString);
+  // print(myBool);
+
+  // Dynamic Keyword
+  // dynamic temp = 1;
+  // print(temp);
+  // temp = 1.3;
+  // print(temp);
+  // temp = "Omkar Gharge";
+  // print(temp);
+  // temp = true;
+  // print(temp);
+
+  //Concatination
+  // var firstname = 'Omkar';
+  // var secondname = 'Gharge';
+  // print(firstname + ' ' + secondname);
+  // print('$firstname $secondname and age is ${4 * 4}');
+
+  // var s = '2';
+  // print(TypeError(s));
+  // var temp = 'abcd';
+  // print(temp.codeUnits);
+  // var tem = 'b';
+  // print(tem.codeUnitAt(0));
+}
 
 // void main() {
 //   int i = 1;
@@ -31,10 +59,9 @@
 //   }
 // }
 
-void main() {
-  int a = 1;
-  var b = 4;
-  b = 3;
+// void main() {
+//   int a = 1;
+//   var b = 4;
+//   b = 3;
 
-
-}
+// }

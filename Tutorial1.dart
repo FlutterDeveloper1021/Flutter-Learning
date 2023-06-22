f47553e905703cad3a9c8b294449035076a320fb
FlutterDeveloper1021 <omkargharge@nimapinfotech.com>
2023-06-22 12:00:22 +0530

    second day commit

---
+++ Tutorial1.dart
@@ -0,0 +1,40 @@
+// void main() {
+//   // print("Hello World!");
+//   // int myInteger = -1;
+//   num myInteger = -1;
+//   double myDouble = 1.2;
+//   String myString = "Omkar Gharge";
+//   bool myBool = true;
+
+//   // if variable hold num as well as double value then use num
+//   myInteger = 1;
+//   print(myInteger);
+
+//   // here ~ this can remove the decimals and only print the integer
+//   num temp = ((4 + 3) ~/ 4);
+//   print(temp);
+//   print(myDouble);
+//   print(myString);
+//   print(myBool);
+// }
+
+// void main() {
+//   int i = 1;
+//   while (i <= 3) {
+//     int j = 1;
+//     while (j <= i) {
+//       print('* ');
+//       j++;
+//     }
+//     print('\n');
+//     i++;
+//   }
+// }
+
+void main() {
+  int a = 1;
+  var b = 4;
+  b = 3;
+
+
+}
